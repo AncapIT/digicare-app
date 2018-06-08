e1c2459c324c21023ad356003852e9583e3ae4b4
RS <abrahamarslan@A2.local>
2018-06-08 19:36:10 +0530

    URL Changes

--- src/models/api-values.ts
+++ src/models/api-values.ts
@@ -5,8 +5,6 @@
 export class VALUES {
 
   // Set Prod or Test server - NOTE: Also change in src/app/api.service.ts
-  //public static API_URL = localStorage.getItem("api_url")
-  //public static API_URL = 'http://test.digicareapp.com/api'
-  //public static API_URL='http://prod.carelink.nu/api';
+
 
 }
